25f6cba630b774f54d9714e63f905374a8e506a4
JedrzejSobkow <sobkowjedrzej@gmail.com>
2025-10-17 22:03:04 +0200

    feat: Added profile picture selector

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 
 const ProfileScreen: React.FC = () => {
     const [description, setDescription] = useState('');
+    const [selectedPictureId, setSelectedPictureId] = useState<number | null>(null);
     const maxChars = 64;
-    const maxRows = 3;
 
     const handleDescriptionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         let value = e.target.value;
@@ -13,68 +13,117 @@ const ProfileScreen: React.FC = () => {
         }
     };
 
+    const handlePictureSelect = (id: number) => {
+        setSelectedPictureId(id);
+    };
+
+    const pictures = [
+        '/assets/images/profile-picture.png',
+        '/assets/images/profile-picture.png',
+        '/assets/images/profile-picture.png',
+        '/assets/images/profile-picture.png',
+        '/assets/images/profile-picture.png',
+        '/assets/images/profile-picture.png',
+        '/assets/images/profile-picture.png',
+        '/assets/images/profile-picture.png',
+        '/assets/images/profile-picture.png',
+        '/assets/images/profile-picture.png',
+        '/assets/images/profile-picture.png',
+        '/assets/images/profile-picture.png',
+        '/assets/images/profile-picture.png',
+        '/assets/images/profile-picture.png',
+        '/assets/images/profile-picture.png',
+        '/assets/images/profile-picture.png',
+    ];
+
     return (
-        <main className="content-section flex flex-col items-center justify-center p-8 gap-8 text-headline font-sans">
-            {/* Profile Picture */}
-            <div className="w-64 h-64 rounded-full bg-gray-400 flex items-center justify-center mb-5 relative">
-                <img
-                    src="/assets/images/profile-picture.png"
-                    alt="Profile Picture"
-                    className="w-60 h-60 rounded-full"
-                />
-            </div>
-
-            {/* Rating */}
-            <div className="text-2xl font-bold text-highlight mb-2">
-                RATING <span className="text-headline pl-2">495.00</span> <span className="text-lp">LP</span>
-            </div>
-
-            {/* Username */}
-            <div className="flex items-center gap-8 text-lg font-normal mb-5">
-                <img
-                    src="/assets/icons/user.png"
-                    alt="User Icon"
-                    className="w-6 h-6"
-                />
-                <span>le_frogger422786</span>
-                <img
-                    src="/assets/icons/edit.png"
-                    alt="Edit Icon"
-                    className="w-6 h-6 cursor-pointer"
-                />
-            </div>
-
-            {/* About Me Section */}
-            <div className="input flex flex-col w-fit static -mt-6">
-                <label
-                    htmlFor="about-me"
-                    className="text-highlight text-xl font-semibold relative top-3 ml-[20px] px-[3px] bg-input-bg w-fit"
-                >
-                    about me:
-                </label>
-                <textarea
-                    id="about-me"
-                    placeholder="Tell us something about you..."
-                    name="about-me"
-                    spellCheck="false"
-                    value={description}
-                    rows={3}
-                    onChange={handleDescriptionChange}
-                    className="input px-[20px] py-[17px] text-s rounded-[20px] w-[350px] focus:outline-none placeholder:text-headline/25 text-headline"
-                    style={{
-                        backgroundColor: 'rgba(47, 46, 54, 0.1)',
-                        border: '3px solid rgba(47, 46, 54, 0.5)',
-                        resize: 'none', 
-                        whiteSpace: 'pre-wrap', 
-                        overflowWrap: 'break-word',
-                    }}
-                ></textarea>
-                {/* Character Counter */}
-                <div className="flex justify-between w-[350px] mt-2">
-                    <div></div> {/* Empty div to push the counter to the right */}
-                    <div className="text-xs text-paragraph">
-                        {maxChars - description.length} characters left
+<main className="content-section flex flex-col items-center justify-center px-0 md:px-16 py-16 gap-8 text-headline font-sans h-full">
+{/* Two Columns Layout */}
+            <div className="flex flex-col lg:flex-row justify-between items-center w-full max-w-6xl gap-8 h-full mx-auto">
+            {/* Left Column: Big Profile Picture, Username, About Me */}
+            <div className="flex flex-col items-center w-4/5 lg:w-1/2 gap-6">
+            {/* Big Profile Picture */}
+                    <div className="w-64 h-64 rounded-full bg-gray-400 flex items-center justify-center relative">
+                        <img
+                            src={selectedPictureId !== null ? pictures[selectedPictureId] : '/assets/images/profile-picture.png'}
+                            alt="Profile Picture"
+                            className="w-60 h-60 rounded-full"
+                        />
                     </div>
+
+                    {/* Username */}
+                    <div className="flex items-center gap-4 text-lg font-normal">
+                        <img
+                            src="/assets/icons/user.png"
+                            alt="User Icon"
+                            className="w-6 h-6"
+                        />
+                        <span>le_frogger422786</span>
+                        <img
+                            src="/assets/icons/edit.png"
+                            alt="Edit Icon"
+                            className="w-6 h-6 cursor-pointer"
+                        />
+                    </div>
+
+                    {/* About Me Section */}
+                    <div className="input flex flex-col w-[60%] min-w-[270px]">
+                    <label
+                            htmlFor="about-me"
+                            className="text-highlight text-xl font-semibold relative top-3 ml-[20px] px-[3px] bg-input-bg w-fit"
+                        >
+                            about me:
+                        </label>
+                        <textarea
+                            id="about-me"
+                            placeholder="Tell us something about you..."
+                            name="about-me"
+                            spellCheck="false"
+                            value={description}
+                            rows={3}
+                            onChange={handleDescriptionChange}
+                            className="input px-[20px] py-[17px] text-s rounded-[20px] w-full focus:outline-none placeholder:text-headline/25 text-headline"
+                            style={{
+                                backgroundColor: 'rgba(47, 46, 54, 0.1)',
+                                border: '3px solid rgba(47, 46, 54, 0.5)',
+                                resize: 'none', 
+                                whiteSpace: 'pre-wrap', 
+                                overflowWrap: 'break-word',
+                            }}
+                        ></textarea>
+                        {/* Character Counter */}
+                        <div className="flex justify-between w-full mt-2">
+                            <div></div> {/* Empty div to push the counter to the right */}
+                            <div className="text-xs text-paragraph">
+                                {maxChars - description.length} characters left
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Right Column: Profile Picture Selection */}
+                <div className="flex flex-col items-center justify-center w-full lg:w-1/2">
+                <h2 className="text-headline text-2xl font-bold mb-4 pb-4">Select your profile picture:</h2>
+                    <div className="grid grid-cols-4 gap-6">
+                        {pictures.map((picture, index) => (
+                            <div
+                                key={index}
+                                className={`w-16 h-16 rounded-full border-2 ${
+                                    selectedPictureId === index
+                                    ? 'border-4 border-highlight'
+                                    : 'border-2 border-gray-400'                                } flex items-center justify-center cursor-pointer`}
+                                onClick={() => handlePictureSelect(index)}
+                            >
+                                <img
+                                    src={picture}
+                                    alt={`Profile Picture ${index + 1}`}
+                                    className="w-full h-full rounded-full"
+                                />
+                            </div>
+                        ))}
+                    </div>
+
+                    
                 </div>
             </div>
         </main>
