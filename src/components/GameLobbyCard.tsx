f9dad4547db855df8c08ee775bb7d15bf940f8d7
JedrzejSobkow <sobkowjedrzej@gmail.com>
2025-10-23 19:37:33 +0200

    fix: Changed layout of player slots

--- src/components/GameLobbyCard.tsx
+++ src/components/GameLobbyCard.tsx
@@ -51,17 +51,17 @@ const GameLobbyCard: React.FC<GameLobbyCardProps> = ({ gameName, lobbyName, game
         <div
           className={`grid gap-4 ${
             maxPlayers === 1
-              ? 'grid-cols-1'
+              ? 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'
               : maxPlayers === 2
-              ? 'grid-cols-2'
+              ? 'grid-cols-2 sm:grid-cols-2 lg:grid-cols-3'
               : maxPlayers === 3
-              ? 'grid-cols-2 sm:grid-cols-3'
+              ? 'grid-cols-2 sm:grid-cols-2 lg:grid-cols-3'
               : maxPlayers === 4
-              ? 'grid-cols-2'
+              ? 'grid-cols-2 sm:grid-cols-2 lg:grid-cols-3'
               : maxPlayers === 5
-              ? 'grid-cols-2 sm:grid-cols-3 sm:grid-rows-2'
+              ? 'grid-cols-2 sm:grid-cols-2 lg:grid-cols-3'
               : maxPlayers === 6
-              ? 'grid-cols-2 sm:grid-cols-3'
+              ? 'grid-cols-2 sm:grid-cols-2 lg:grid-cols-3'
               : ''
           }`}
         >
