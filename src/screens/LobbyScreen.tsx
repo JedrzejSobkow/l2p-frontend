82529d4b073eafefc57e87e0d4e0336f54a2fe92
JedrzejSobkow <sobkowjedrzej@gmail.com>
2025-12-01 12:27:46 +0100

    On smaller screen instead of tooltip show info inside of the start button

--- src/screens/LobbyScreen.tsx
+++ src/screens/LobbyScreen.tsx
@@ -480,21 +480,16 @@ export const LobbyScreen = () => {
             </button>
 
             {isUserHost && (
-              <div className="w-full relative group">
-                <button 
-                  disabled={!canStartGame} 
-                  onClick={handleStartGame}
-                  className="w-full px-4 py-3 bg-blue-500 text-white font-bold text-base rounded-lg hover:bg-blue-600 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-blue-500"
-                >
-                  Start
-                </button>
+              <button 
+                disabled={!canStartGame} 
+                onClick={handleStartGame}
+                className="w-full px-4 py-3 bg-blue-500 text-white font-bold text-base rounded-lg hover:bg-blue-600 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-blue-500 flex flex-col items-center gap-1"
+              >
+                <span>Start</span>
                 {!canStartGame && startButtonTooltip && (
-                  <div className="hidden group-hover:block absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-2 bg-gray-800 text-white text-sm rounded-lg whitespace-nowrap z-10 shadow-lg">
-                    {startButtonTooltip}
-                    <div className="absolute top-full left-1/2 transform -translate-x-1/2 -mt-1 border-4 border-transparent border-t-gray-800"></div>
-                  </div>
+                  <span className="text-xs font-normal opacity-90">{startButtonTooltip}</span>
                 )}
-              </div>
+              </button>
             )}
           </div>
 
