ed41455dd1d584a73d261ce98dbfd6f246fc9a1c
Damian Kwolek <damiankwolek1@gmail.com>
2025-12-01 15:35:51 +0100

    fix: popup UI, friends events handlers

--- src/components/friends/FriendsContext.tsx
+++ src/components/friends/FriendsContext.tsx
@@ -16,6 +16,7 @@ import {
   getFriendStatus,
   searchFriends,
   sendFriendRequest,
+  type FriendResult,
   type Friendship,
   type FriendshipStatus,
   type SearchFriendsPayload,
@@ -44,6 +45,7 @@ import {
   offFriendRequestAccepted
 } from '@/services/chat'
 import { useGlobalError } from '../GlobalErrorContext'
+import { usePopup } from '../PopupContext'
 
 export type Friend = {
   id: string,
@@ -73,7 +75,8 @@ type FriendsContextValue = {
   friends: Friend[]
   incomingRequests: Friend[]
   outgoingRequests: Friend[]
-  sendRequest: (friendId: number | string, nickname: string, avatarUrl: string, description: string) => Promise<void>
+  processingMap?: Record<string, boolean>
+  sendRequest: (user: FriendResult) => Promise<void>
   acceptRequest: (friendId: number | string) => Promise<void>
   declineRequest: (friendId: number | string) => Promise<void>
   removeFriend: (friendId: number | string) => Promise<void>
@@ -87,6 +90,12 @@ export const FriendsProvider = ({ children }: { children: ReactNode }) => {
   const [isLoading, setIsLoading] = useState<boolean>(true)
   const [state, setState] = useState<FriendsState>({ friendsById: {} })
   const { triggerError } = useGlobalError()
+  const {showPopup} = usePopup()
+  const [processingMap, setProcessingMap] = useState<Record<string, boolean>>({})
+
+  const markProcessing = (id: string, value: boolean) => {
+    setProcessingMap((prev) => ({ ...prev, [id]: value }))
+  }
 
   const handleInitialFriendStatuses = useCallback((payload:{statuses:  FriendStatusUpdatePayload[]}) => {
     if (!Array.isArray(payload.statuses)) return
@@ -242,10 +251,13 @@ export const FriendsProvider = ({ children }: { children: ReactNode }) => {
 
   // }, [])
 
-  const sendRequestHandler = useCallback(async (friendId: number | string, nickname: string, avatarUrl: string, description: string) => {
-    await sendFriendRequest(friendId)
+  const sendRequestHandler = useCallback(async (user: FriendResult) => {
+    const { user_id: friendId, nickname, pfp_path: avatarUrl, description } = user
     const key = String(friendId)
-    setState((prev) => ({
+    markProcessing(key, true)
+    try {
+      await sendFriendRequest(key)
+      setState((prev) => ({
       friendsById: {
         ...prev.friendsById,
         [key]: {
@@ -259,60 +271,120 @@ export const FriendsProvider = ({ children }: { children: ReactNode }) => {
         }
       },
     }))
+      showPopup({ type: 'confirmation', message: `Friend request sent to ${nickname}.` })
+    } catch (error: any) {
+      showPopup({ type: 'error', message: error.message || 'Failed to send friend request. Please try again later.' })
+      throw error
+    } finally {
+      markProcessing(key, false)
+    }
   }, [])
 
   const acceptRequestHandler = useCallback(async (friend_user_id: number | string) => {
-    await acceptFriendRequest(friend_user_id)
     const key = String(friend_user_id)
-    const status = await getFriendStatus(key)
-    setState((prev) => ({
-      friendsById: {
-        ...prev.friendsById,
-        [key]: {
-          ...prev.friendsById[key],
-          friendShipStatus: 'accepted',
-          isRequester: false,
-          userStatus: status.status as UserStatus,
-          gameName: status.game_name,
-          lobbyCode: status.lobby_code,
-          lobbyFilledSlots: status.lobby_filled_slots,
-          lobbyMaxSlots: status.lobby_max_slots,
-        }
-      },
-    }))
-
-            
+    try {
+      markProcessing(key, true)
+      await acceptFriendRequest(key)
+      const status = await getFriendStatus(key)
+      setState((prev) => ({
+        friendsById: {
+          ...prev.friendsById,
+          [key]: {
+            ...prev.friendsById[key],
+            friendShipStatus: 'accepted',
+            isRequester: false,
+            userStatus: status.status as UserStatus,
+            gameName: status.game_name,
+            lobbyCode: status.lobby_code,
+            lobbyFilledSlots: status.lobby_filled_slots,
+            lobbyMaxSlots: status.lobby_max_slots,
+          }
+        },    
+      }))
+      showPopup({ type: 'confirmation', message: 'Friend request accepted.' })
+    } catch (error) {
+      showPopup({ type: 'error', message: 'Failed to accept friend request. Please try again later.' })
+    } finally {
+      markProcessing(key, false)
+    }  
   }, [])
 
   const declineRequestHandler = useCallback(async (friend_user_id: number | string) => {
-    await declineFriendRequest(friend_user_id)
+    const key = String(friend_user_id)
+    try {
+      markProcessing(key, true)
+      await declineFriendRequest(friend_user_id)
+      setState((prev) => {
+        const newFriendsById = { ...prev.friendsById }
+        delete newFriendsById[key]
+        return { friendsById: newFriendsById }
+      })
+      
+      showPopup({ type: 'confirmation', message: 'Friend request declined.' })
+    } catch (error) {
+      showPopup({ type: 'error', message: 'Failed to decline friend request. Please try again later.' })
+      
+    } finally {
+      markProcessing(key, false)
+
+    }
   }, [])
 
   const removeFriendHandler = useCallback(async (friend_user_id: number | string) => {
-    await deleteFriend(friend_user_id)
+    try {
+      await deleteFriend(friend_user_id)
+      showPopup({ type: 'confirmation', message: 'Friend removed successfully.' })
+      setState((prev) => {
+        const key = String(friend_user_id)
+        const newFriendsById = { ...prev.friendsById }
+        delete newFriendsById[key]
+        return { friendsById: newFriendsById }
+      })
+    }
+    catch (error) {
+      showPopup({ type: 'error', message: 'Failed to remove friend. Please try again later.' })
+      throw error
+    }
   }, [])
 
   const handleFriendRequestAccepted = useCallback(async (payload: FriendRequestAccepted) => {
     const key = String(payload.accepter_id)
-    const friendStatus = await getFriendStatus(key)
-    setState((prev) => ({
-      friendsById: {
-        ...prev.friendsById,
-        [key]: {
-          ...prev.friendsById[key],
-          id: key,
-          nickname: payload.accepter_nickname,
-          avatarUrl: payload.accepter_pfp_path || pfpImage,
-          friendShipStatus: 'accepted',
-          isRequester: false,
-          userStatus: friendStatus.status as UserStatus,
-          gameName: friendStatus.game_name,
-          lobbyCode: friendStatus.lobby_code,
-          lobbyFilledSlots: friendStatus.lobby_filled_slots,
-          lobbyMaxSlots: friendStatus.lobby_max_slots,
-        }
-      },
-    }))
+    try {
+      const friendStatus = await getFriendStatus(key)
+      setState((prev) => ({
+        friendsById: {
+          ...prev.friendsById,
+          [key]: {
+            ...prev.friendsById[key],
+            id: key,
+            nickname: payload.accepter_nickname,
+            avatarUrl: payload.accepter_pfp_path || pfpImage,
+            friendShipStatus: 'accepted',
+            isRequester: false,
+            userStatus: friendStatus.status as UserStatus,
+            gameName: friendStatus.game_name,
+            lobbyCode: friendStatus.lobby_code,
+            lobbyFilledSlots: friendStatus.lobby_filled_slots,
+            lobbyMaxSlots: friendStatus.lobby_max_slots,
+          }
+        },
+      }))
+    } catch (error) {
+      console.error('Error fetching friend status after request accepted:', error)
+      setState((prev) => ({
+        friendsById: {
+          ...prev.friendsById,
+          [key]: {
+            ...prev.friendsById[key],
+            id: key,
+            nickname: payload.accepter_nickname,
+            avatarUrl: payload.accepter_pfp_path || pfpImage,
+            friendShipStatus: 'accepted',
+            isRequester: false,
+          }
+        },
+      }))
+    }
   }, [])
 
   const searchUsersHandler = useCallback(
@@ -414,6 +486,7 @@ export const FriendsProvider = ({ children }: { children: ReactNode }) => {
       friendsById: state.friendsById,
       incomingRequests,
       outgoingRequests,
+      processingMap: processingMap,
       sendRequest: sendRequestHandler,
       acceptRequest: acceptRequestHandler,
       declineRequest: declineRequestHandler,
@@ -428,6 +501,7 @@ export const FriendsProvider = ({ children }: { children: ReactNode }) => {
       incomingRequests,
       isLoading,
       outgoingRequests,
+      processingMap,
       removeFriendHandler,
       searchUsersHandler,
       sendRequestHandler,
