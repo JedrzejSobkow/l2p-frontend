660f8bc0a042c1f16a6e5a1c3106166609523194
Damian Kwolek <damiankwolek1@gmail.com>
2025-12-07 16:04:15 +0100

    fix: not showing overflow chats

--- src/components/chat/ChatDock.tsx
+++ src/components/chat/ChatDock.tsx
@@ -19,10 +19,8 @@ const getMaxVisibleWindows = () => {
 }
 
 const ChatDock = () => {
-  const { sessions, minimizeChat, closeChat } = useChatDock()
+  const { sessions } = useChatDock()
   const { isAuthenticated } = useAuth()
-  const chat = useChat()
-  const {friendsById} = useFriends()
   const location = useLocation()
   
   const [maxVisible, setMaxVisible] = useState<number>(() => getMaxVisibleWindows())
@@ -63,7 +61,7 @@ const ChatDock = () => {
             <DockItem
               key={s.id}
               userId={s.id}
-              minimized={s.minimized}
+              minimized={true}
             />
           )}
         </div>
